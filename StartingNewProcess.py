1e42d12d50d29e0c9286beb451ba20f827dc9563
Vitali's Repo <vkremez@users.noreply.github.com>
2016-05-04 03:05:58 -0400

    Create StartingNewProcess.py

---
+++ StartingNewProcess.py
@@ -0,0 +1,8 @@
+from winappdbg import System import sys
+# Instance a System object.
+system = System()
+# Get the target application.
+command_line = system.argv_to_cmdline( sys.argv[ 1 : ] ) # Start a new process.
+process = system.start_process( command_line ) # see the docs for more options
+# Show info on the new process.
+print "Started process %d (%d bits)" % ( process.get_pid(), process.get_bits() )
