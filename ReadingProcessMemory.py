5bbd697a6a054b5290dd823f63177e4ccaa25e10
Vitali's Repo <vkremez@users.noreply.github.com>
2016-05-04 03:26:44 -0400

    Update ReadingProcessMemory.py

--- ReadingProcessMemory.py
+++ ReadingProcessMemory.py
@@ -1,10 +1,10 @@
 from winappdbg import Process
 def process_read( pid, address, length ):
     # Instance a Process object.
-process = Process( pid )
+    process = Process( pid )
 # Read the process memory.
-data = process.read( address, length )
+    data = process.read( address, length )
 # You can also change the process memory.
     # process.write( address, "example data" )
     # Return a Python string with the memory contents.
-return data
+    return data
